30e80a8a8c33f061873ae0cedd900a41109fcf03
yangfch <yangfch@androidex.cn>
2016-12-12 19:14:59 +0800

    更新nfc开门部分代码

--- aexNFC/src/main/java/com/example/androidex/MainActivity.java
+++ aexNFC/src/main/java/com/example/androidex/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.androidex;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Build;
@@ -9,21 +14,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Window;
 
+import com.androidex.common.SoundPoolUtil;
+import com.androidex.devices.aexddAndroidNfcReader;
+import com.androidex.devices.appDevicesManager;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
  *
  */
-public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback{
-    private static final String TAG= "MainActivity";
+public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
+    private static final String TAG = "MainActivity";
     public static int READER_FLAGS =
-            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK ;
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    appDevicesManager mDevices ;
 
+    public Mbrocast mb;
     //public LoyaltyCardReader mLoyaltyCardReader;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        mDevices = new appDevicesManager(this);
+        mb = new Mbrocast();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(aexddAndroidNfcReader.START_ACTION);
+        registerReceiver(mb,intentFilter);
     }
 
     @Override
@@ -40,21 +69,20 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
 
     private void enableReaderMode() {
         Log.i("", "启用读卡模式");
-        if(Build.VERSION.SDK_INT >= 19)
-        {
+        if (Build.VERSION.SDK_INT >= 19) {
             Activity activity = this;
             NfcAdapter nfc = NfcAdapter.getDefaultAdapter(activity);
             if (nfc != null) {
-                //nfc.enableReaderMode(activity, , READER_FLAGS, null);
+                nfc.enableReaderMode(activity, (aexddAndroidNfcReader)mDevices.mNfcReader, READER_FLAGS, null);
             }
         }
     }
 
-    private void disenableReaderMode(){
-        Log.i("","禁用读卡模式");
+    private void disenableReaderMode() {
+        Log.i("", "禁用读卡模式");
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if(nfcAdapter!=null){
-            if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+        if (nfcAdapter != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 nfcAdapter.disableReaderMode(this);
             }
         }
@@ -63,5 +91,60 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
     @Override
     public void onTagDiscovered(Tag tag) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mb);
+    }
+
+    /**
+     * 广播
+     */
+    public class Mbrocast extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (aexddAndroidNfcReader.START_ACTION.equals(action)){
+                SoundPoolUtil.getSoundPoolUtil().loadVoice(MainActivity.this,4681);
+            }
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
