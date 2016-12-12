c11a21fe0f54b0023832f00b72dcb2c80ad73de2
hockgod <liyp@androidex.cn>
2016-12-12 20:52:26 +0800

    给Log页面增加点击显示/隐藏Toolbar效果（只可点击显示Log的控件）

--- aexHome/src/main/java/com/androidex/apps/home/aexLogFragment.java
+++ aexHome/src/main/java/com/androidex/apps/home/aexLogFragment.java
@@ -1,5 +1,6 @@
 package com.androidex.apps.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -7,15 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.androidex.common.LogFragment;
+import com.androidex.common.OnMultClickListener;
 import com.androidex.logger.Log;
 import com.androidex.logger.LogWrapper;
 import com.androidex.logger.MessageOnlyLogFilter;
+
+import static com.androidex.apps.home.AdvertFragment.ONCLICKTIMES;
 
 /**
  * Created by yangjun on 2016/11/7.
  */
 
-public class aexLogFragment extends LogFragment {
+public class aexLogFragment extends LogFragment implements OnMultClickListener {
     public static final String TAG = "LOG";
 
     public aexLogFragment() {
@@ -35,6 +39,7 @@ public class aexLogFragment extends LogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initializeLogging();
+        FullscreenActivity.registerMultClickListener(getLogView(), this);
     }
 
     /** Create a chain of targets that will receive log data
@@ -46,5 +51,17 @@ public class aexLogFragment extends LogFragment {
         MessageOnlyLogFilter msgFilter = (MessageOnlyLogFilter)logWrapper.getNext();
         msgFilter.setNext(this.getLogView());
         Log.i(TAG,"就绪");
+    }
+
+    @Override
+    public boolean OnMultClick(View view, int times) {
+        if (times == ONCLICKTIMES) {
+            Intent intent = new Intent(FullscreenActivity.ActionControlBar);
+            intent.putExtra("flag", "toggle");
+            intent.putExtra("bar", true);
+            getLogView().getContext().sendBroadcast(intent);
+            return true;
+        }
+        return false;
     }
 }
