060375dc5a2992748e4434a8504ab71e21f4fab8
hockgod <liyp@androidex.cn>
2016-12-15 22:09:25 +0800

    更新recorvoiceFragment

--- aexHome/src/main/java/com/androidex/apps/home/Fragment/RecordVoiceFragment.java
+++ aexHome/src/main/java/com/androidex/apps/home/Fragment/RecordVoiceFragment.java
@@ -1,76 +1,153 @@
 package com.androidex.apps.home.fragment;
 
-import android.net.Uri;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.androidex.apps.home.FullscreenActivity;
 import com.androidex.apps.home.R;
-import com.androidex.apps.home.view.CustomVideoView;
+
+import java.io.IOException;
 
 /**
  * Created by liyp on 16/12/10.
  */
 
-public class RecordVoiceFragment extends LazyLoadFragment implements View.OnClickListener {
+public class RecordVoiceFragment extends DialogFragment implements View.OnClickListener {
+    private static final String TAG = "setpassword";
+    private View rootView;
+    private static RecordVoiceFragment recordVoiceFragment;
     private FullscreenActivity activity;
-    private int index;
-    private ImageView iv_close;
-    private CustomVideoView customVideoView;
+    public boolean isCancelable = false;
+    //语音操作对象
+    private MediaPlayer mPlayer = null;
+    private MediaRecorder mRecorder = null;
 
-    public RecordVoiceFragment(){
+    //语音文件保存路径
+    private String FileName = null;
 
-    }
+    private boolean flag =true; //控制按钮是播放还是结束
+    
+    @Nullable
     @Override
-    protected void lazyLoad() {
-        iniView();
-
-        activity = (FullscreenActivity) getActivity();
-        /**获取参数，根据不同的参数播放不同的视频**/
-        index = getArguments().getInt("index");
-        Log.d("vediofragment","开始播放视频="+index);
-        Uri uri;
-        if (index == 1) {
-            uri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.a002);
-        } else {
-            uri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.love);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_recordvoice, container, false);
         }
-        /**播放视频**/
-        customVideoView.setVisibility(View.VISIBLE);
-        customVideoView.playVideo(uri);
+        setCancelable(isCancelable);
+        //设置sdcard的路径
+        FileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        FileName += "/audiorecord.3gp";
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        activity = (FullscreenActivity) getActivity();
+        initView();
+        return rootView;
     }
 
-    public void iniView() {
-        iv_close = findViewById(R.id.iv_close);
-        customVideoView = findViewById(R.id.cv);
-        iv_close.setOnClickListener(this);
+    public void initView() {
+        ImageView stop = (ImageView) rootView.findViewById(R.id.iv_stop);
+        ImageView play = (ImageView) rootView.findViewById(R.id.iv_play);
+        ImageView record = (ImageView) rootView.findViewById(R.id.iv_record);
+        stop.setOnClickListener(this);
+        play.setOnClickListener(this);
+        record.setOnClickListener(this);
     }
 
-    public RecordVoiceFragment dissMissDialog() {
-        dismiss();
-        return this;
-    }
+    protected void showToast(String message) {
+        if (!TextUtils.isEmpty(message)) {
+            Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+        }
 
-    @Override
-    protected int setContentView() {
-        return R.layout.fragment_vedio;
+    }
+    public static RecordVoiceFragment instance() {
+        if (recordVoiceFragment == null) {
+            recordVoiceFragment = new RecordVoiceFragment();
+        }
+        return recordVoiceFragment;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_close:
-                dissMissDialog();
+        switch (v.getId()){
+            case R.id.iv_stop:
+                stopRecord();
+                break;
+
+            case R.id.iv_play:
+                startPlayRecord();
+                break;
+
+            case R.id.iv_record:
+                startRecord();
                 break;
         }
+
     }
-    @Override
-    protected void stopLoad() {
-        super.stopLoad();
-        if (customVideoView != null) {
-            customVideoView.stopPlayback();
-            customVideoView.setVisibility(View.GONE);
+
+    /**
+     * 开始录音
+     */
+    public  void startRecord(){
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(FileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e("RecordVoice","RecordVoice Fail");
         }
+        mRecorder.start();
+    }
+
+    /**
+     * 停止录音
+     */
+    public void stopRecord(){//录完音之后才能去执行
+        if(mRecorder!=null){
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+    }
+
+    /**
+     * 开始播放录音
+     */
+    public void startPlayRecord(){
+        mPlayer = new MediaPlayer();
+        if(FileName!=null){
+            try{
+                mPlayer.setDataSource(FileName);
+                mPlayer.prepare();
+                mPlayer.start();
+            }catch(IOException e){
+                Log.e("RecordVoice","RecordPlay Fail");
+            }
+        }
+    }
+
+    /**
+     * 停止播放录音
+     */
+    public void stopPlayRecord(){
+        if (mPlayer!=null){
+            mPlayer.release();
+            mPlayer = null;
+        }
+
     }
 }
