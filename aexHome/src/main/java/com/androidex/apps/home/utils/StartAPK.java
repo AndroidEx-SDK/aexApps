448f9e3e9466e826743f2de62d48067a9a3fa6e3
hockgod <liyp@androidex.cn>
2017-02-18 18:13:18 +0800

    增加根据包名启动apk

---
+++ aexHome/src/main/java/com/androidex/apps/home/utils/StartAPK.java
@@ -0,0 +1,79 @@
+package com.androidex.apps.home.utils;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Created by cts on 17/2/18.
+ */
+
+public class StartAPK {
+    /**
+     * 启动apk
+     * @param packageName
+     * @param context
+     */
+    public static void RunApp(String packageName, Context context) {
+        PackageInfo pi;
+        try {
+            pi = context.getPackageManager().getPackageInfo(packageName, 0);
+            Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+            resolveIntent.setPackage(pi.packageName);
+            PackageManager pManager = context.getPackageManager();
+            List<ResolveInfo> apps = pManager.queryIntentActivities(
+                    resolveIntent, 0);
+
+            ResolveInfo ri = apps.iterator().next();
+            if (ri != null) {
+                packageName = ri.activityInfo.packageName;
+                String className = ri.activityInfo.name;
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                ComponentName cn = new ComponentName(packageName, className);
+                intent.setComponent(cn);
+                context.startActivity(intent);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 根据包名启动里面的activity
+     * @param packageName
+     * @param context
+     */
+    private void RunAppActivity(String packageName, Context context) {
+        PackageInfo pi;
+        try {
+            pi = context.getPackageManager().getPackageInfo(packageName, 0);
+            Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+            resolveIntent.setPackage(pi.packageName);
+            PackageManager pManager = context.getPackageManager();
+            List<ResolveInfo> apps = pManager.queryIntentActivities(
+                    resolveIntent, 0);
+
+            ResolveInfo ri = apps.iterator().next();
+            if (ri != null) {
+                packageName = ri.activityInfo.packageName;
+                String className = ri.activityInfo.name;
+
+                Intent intent = new Intent();
+
+                ComponentName cn = new ComponentName(packageName, "org.xx.cn.Activity2");
+                intent.setComponent(cn);
+                intent.setAction("android.intent.action.VIEW");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
