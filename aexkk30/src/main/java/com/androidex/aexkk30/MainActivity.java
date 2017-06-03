3f15fb00be47b68896ffef2b0ca8e890ade6d341
hockgod <liyp@androidex.cn>
2017-06-03 14:50:38 +0800

    新增KK30的model,用来做KK30A10测试使用

---
+++ aexkk30/src/main/java/com/androidex/aexkk30/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidex.aexkk30;
+
+import android.os.Bundle;
+
+import com.androidex.common.AndroidExActivityBase;
+
+public class MainActivity extends AndroidExActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
