a807c066ad5eb657df8a6b7a80898c1c918b6a4e
liyp <liyp@androidex.cn>
2017-08-13 23:13:57 +0800

    no message

---
+++ aexSerialAssistant/src/main/java/com/androidex/bean/ComBean.java
@@ -0,0 +1,30 @@
+package com.androidex.bean;
+
+/**
+ * Created by cts on 17/3/31.
+ */
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author benjaminwan
+ *串口数据
+ */
+public class ComBean {
+    public byte[] bRec=null;
+    public String sRecTime="";
+    public String sComPort="";
+    public ComBean(String sPort, byte[] buffer, int size){
+        sComPort=sPort;
+        bRec=new byte[size];
+        for (int i = 0; i < size; i++)
+        {
+            bRec[i]=buffer[i];
+        }
+        SimpleDateFormat sDateFormat = new SimpleDateFormat("hh:mm:ss");
+        sRecTime = sDateFormat.format(new java.util.Date());
+    }
+    public void poll(){
+
+    }
+}
