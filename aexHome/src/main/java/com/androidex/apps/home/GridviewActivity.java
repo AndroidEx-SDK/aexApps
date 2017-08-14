f15ea6cf746dcf794cf97a778a590dc9cce72022
zhanghang <517648640@qq.com>
2017-08-14 18:34:15 +0800

    在home中新增GridViewActivity

---
+++ aexHome/src/main/java/com/androidex/apps/home/GridviewActivity.java
@@ -0,0 +1,88 @@
+package com.androidex.apps.home;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GridviewActivity extends Activity {
+    private GridView gview;
+    private List<Map<String, Object>> data_list;
+    private SimpleAdapter sim_adapter;
+    // 图片封装为一个数组
+    private int[] icon = { R.drawable.balance, R.drawable.balance,
+            R.drawable.balance, R.drawable.ic_launcher, R.drawable.balance,
+            R.drawable.balance, R.drawable.balance, R.drawable.balance,
+            R.drawable.balance, R.drawable.balance, R.drawable.balance,
+            R.drawable.balance };
+    private String[] iconName = { "item1", "item2", "item3", "item4", "item5", "item6", "item7",
+            "item8", "item9", "item10", "item11", "item12" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview);
+        gview = (GridView) findViewById(R.id.gview);
+        //新建List
+        data_list = new ArrayList<Map<String, Object>>();
+        //获取数据
+        getData();
+        //新建适配器
+        String [] from ={"image","text"};
+        int [] to = {R.id.image,R.id.text};
+        sim_adapter = new SimpleAdapter(this, data_list, R.layout.item, from, to);
+        //配置适配器
+        gview.setAdapter(sim_adapter);
+        gview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Toast.makeText(GridviewActivity.this, "item1", Toast.LENGTH_LONG)
+                                .show();
+                        break;
+                    case 1:
+                        Toast.makeText(GridviewActivity.this, "item2", Toast.LENGTH_LONG)
+                                .show();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+
+    public List<Map<String, Object>> getData(){
+        //cion和iconName的长度是相同的，这里任选其一都可以
+        for(int i=0;i<icon.length;i++){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("image", icon[i]);
+            map.put("text", iconName[i]);
+            data_list.add(map);
+        }
+        return data_list;
+    }
+
+
+}
